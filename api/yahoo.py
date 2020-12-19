6feecaddadc723e8e0a4a05d5eafca69c10e3453
SuperCheese21 <ethan.shields21@gmail.com>
2020-12-19 03:28:21 -0800

    Add matchup fetch function

--- api/yahoo.py
+++ api/yahoo.py
@@ -4,13 +4,35 @@ from yahoo_oauth import OAuth2
 oauth = OAuth2(None, None, from_file="yahooAuth.json")
 
 
-def main():
-    league_ids = game.Game(oauth, "nba").league_ids()
-    print(league_ids)
-    current_league = league.League(oauth, "402.l.35721")
-    print(current_league.matchups())
-    return 0
+def get_league_id():
+    current_game = game.Game(oauth, "nba")
+    league_ids = current_game.league_ids()
+    if not league_ids:
+        return None
+    return league_ids[len(league_ids) - 1]
 
 
-if __name__ == "__main__":
-    main()
+def get_team_key(team):
+    team_data = team["team"][0]
+    for line in team_data:
+        if "team_key" in line:
+            return line["team_key"]
+    return None
+
+
+def get_matchups(week):
+    league_id = get_league_id()
+    current_league = league.League(oauth, league_id)
+    matchups = current_league.matchups(week)
+    matchups_dict = matchups["fantasy_content"]["league"][1]["scoreboard"]["0"][
+        "matchups"
+    ]
+    team_key_tuples = []
+    for key, value in matchups_dict.items():
+        if not isinstance(value, dict):
+            continue
+        teams_dict = value["matchup"]["0"]["teams"]
+        team_key_tuples.append(
+            (get_team_key(teams_dict["0"]), get_team_key(teams_dict["1"]))
+        )
+    return team_key_tuples
