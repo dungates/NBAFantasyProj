f27912193a783bb782625917cb46f82789210757
SuperCheese21 <ethan.shields21@gmail.com>
2020-12-25 01:47:50 -0800

    Refactoring

--- src/age_analysis.py
+++ src/age_analysis.py
@@ -3,17 +3,6 @@ from constants import STAT_COEFFS
 from utils import get_averages, inject_fantasy_points
 
 
-def get_totals(player_seasons):
-    totals = {}
-    keys = ["GP", "FP"]
-    for key in keys + list(STAT_COEFFS.keys()):
-        totals[key] = 0
-    for player_season in player_seasons:
-        for key, total in totals.items():
-            totals[key] = total + player_season[key]
-    return totals
-
-
 def analyze_data(data):
     age_data = {}
     for age in sorted(data.keys()):
@@ -31,6 +20,21 @@ def analyze_data(data):
             + str(totals["GP"])
         )
         age_data[age] = {"totals": totals, "averages": averages}
+    write_data(age_data)
+
+
+def get_totals(player_seasons):
+    totals = {}
+    keys = ["GP", "FP"]
+    for key in keys + list(STAT_COEFFS.keys()):
+        totals[key] = 0
+    for player_season in player_seasons:
+        for key, total in totals.items():
+            totals[key] = total + player_season[key]
+    return totals
+
+
+def write_data(age_data):
     with open("Data/statsByAge.json", "w") as json_file:
         json.dump(age_data, json_file)
 
--- src/better_age_analysis.py
+++ src/better_age_analysis.py
@@ -1,20 +1,10 @@
-from constants import SEASON_WEIGHTS
 import json
-from pydash.numerical import mean
+from pydash.numerical import mean, std_deviation
+from constants import SEASON_WEIGHTS
 from utils import get_season_qualified, inject_fantasy_points
 
 
 num_past_seasons = len(SEASON_WEIGHTS)
-
-
-def get_weighted_average(player_seasons):
-    total = 0
-    for index, weight in enumerate(SEASON_WEIGHTS):
-        player_season = player_seasons[index]
-        if not get_season_qualified(player_season):
-            return None
-        total = total + weight * player_season["FP"] / player_season["GP"]
-    return total / sum(SEASON_WEIGHTS)
 
 
 def analyze_data(data):
@@ -42,13 +32,35 @@ def analyze_data(data):
                 ratios[player_age] = []
 
             current_average = player_season["FP"] / player_season["GP"]
-
             ratios[player_age].append(current_average / past_average)
 
+    write_data(ratios)
+
+
+def get_weighted_average(player_seasons):
+    total = 0
+    for index, weight in enumerate(SEASON_WEIGHTS):
+        player_season = player_seasons[index]
+        if not get_season_qualified(player_season):
+            return None
+        total = total + weight * player_season["FP"] / player_season["GP"]
+    return total / sum(SEASON_WEIGHTS)
+
+
+def write_data(ratios):
     f = open("Data/player_age_averages.txt", "w")
     for age in sorted(ratios.keys()):
         values = ratios[age]
-        f.write(str(age) + ": " + str(mean(values)) + ", " + str(len(values)) + "\n")
+        f.write(
+            str(age)
+            + ": "
+            + str(mean(values))
+            + ", "
+            + str(std_deviation(values))
+            + ", "
+            + str(len(values))
+            + "\n"
+        )
     f.close()
 
 
