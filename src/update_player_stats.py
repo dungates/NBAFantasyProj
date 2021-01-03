bb43208664685dde235d5ef1878e1c125ff72c83
SuperCheese21 <ethan.shields21@gmail.com>
2021-01-03 14:20:27 -0800

    Added comments to player stats update script

--- src/update_player_stats.py
+++ src/update_player_stats.py
@@ -6,9 +6,17 @@ from utils import write_json
 
 
 if __name__ == "__main__":
+    # Get current NBA season
     current_season = get_current_season()
+
+    # Fetch player season data for last 20 seasons
     results = get_player_seasons(current_season, 20)
+
+    # Order results by player age
+    order_by(results, "AGE")
+
+    # Group data by player
     grouped_data = group_by(results, "PLAYER_ID")
-    for value in grouped_data.values():
-        order_by(value, "AGE")
+
+    # Write data to JSON file
     write_json(grouped_data, "seasonTotalsByPlayer")
