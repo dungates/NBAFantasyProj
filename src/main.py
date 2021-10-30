f3104bfa8cdf6ab195c1ae440d982ea64a7f4ef6
SuperCheese21 <ethan.shields21@gmail.com>
2021-10-29 17:05:42 -0700

    Added error handling to account adder

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+from sys import platform
 from constants import LEAGUE_TYPES
 from utils import get_config_files, option_selector, write_json
 
@@ -10,19 +11,36 @@ def add_fantasy_account():
         lambda league_type: league_type["name"],
     )
     selected_key = selected_option[1]["key"]
-    config_path = f"config/{selected_key}/"
-    os.makedirs(config_path, exist_ok=True)
 
     name = input("Enter account name: ")
+    config_file_dir = f"config/{selected_key}"
+    config_file_name = f"{config_file_dir}/{name}.json"
+    if os.path.exists(config_file_name):
+        print(f"Account with name '{name}' already exists!")
+        return
+
+    os.makedirs(config_file_dir, exist_ok=True)
+
+    with os.scandir(config_file_dir) as entries:
+        for file in entries:
+            if file.is_file() and file.name.endswith(".json"):
+                print(file.name)
+
     if selected_key == "yahoo":
         key = input("Enter consumer key: ")
         secret = input("Enter consumer secret: ")
-        os.system(f"yfa_init_oauth_env -k {key} -s {secret} {config_path}{name}.json")
+        command = (
+            f"python {os.getenv('VIRTUAL_ENV')}/env/Scripts/yfa_init_oauth_env"
+            if platform == "win32"
+            else "yfa_init_oauth_env"
+        )
+        args = f"-k {key} -s {secret} {config_file_dir}/{name}.json"
+        os.system(f"{command} {args}")
     elif selected_key == "espn":
         espn_s2 = input("Enter espn_s2 variable: ")
         swid = input("Enter swid variable: ")
         json_object = {"espn_s2": espn_s2, "swid": swid}
-        write_json(json_object, f"{config_path}{name}.json")
+        write_json(json_object, f"{config_file_dir}/{name}.json")
 
     main()
 
