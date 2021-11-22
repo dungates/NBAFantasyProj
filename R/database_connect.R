3cf0f24edb6db34964c308948009d7f954346be4
dungates <duncan.gates123@gmail.com>
2021-11-21 22:39:19 -0800

    add player_tables

--- R/database_connect.R
+++ R/database_connect.R
@@ -1,5 +1,10 @@
-con <- DBI::dbConnect(RSQLite::SQLite(), dbname = "nba.db")
-library(magrittr)
-upload_data <- readr::read_csv("Data/player_season_totals.csv")
+# Duplicate in data folder and database
+database_connect <- function(upload_data, name) {
+  con <- DBI::dbConnect(RSQLite::SQLite(), dbname = "nba.db")
+  
+  # upload_data <- readr::write_csv(paste0("Data/", name, ".csv"))
+  
+  RSQLite::dbWriteTable(conn = con, name = name, value = upload_data, overwrite = TRUE)
+}
 
-RSQLite::dbWriteTable(conn = con, name = "player_seasons", value = upload_data, overwrite = TRUE)
+
